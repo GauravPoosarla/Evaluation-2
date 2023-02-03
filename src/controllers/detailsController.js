35210c4500caf2e945582a7f09b71745d9e29173
GauravPoosarla <poosarlagaurav@gmail.com>
2023-02-03 15:45:25 +0530

    feat: add route for updating details

--- src/controllers/detailsController.js
+++ src/controllers/detailsController.js
@@ -4,13 +4,22 @@ let userDetails;
 exports.detailsController = async (req, res) => {
   const urlLink = req.body.urlLink;
   const result = await services.postDetails(urlLink);
-  console.log(result);
-  res.status(200).json(result);
+  const finalResult = [];
+  result.map((item) => (finalResult.push({id: item.id, name: item.name, score: item.score  })));
+  res.status(200).json(finalResult);
 }
 
 exports.getCompanies = async (req, res) => {
   const sector = req.query.sector;
   const result = await services.getCompanies(sector);
-  res.status(200).json(result);
+  const finalResult = [];
+  result.map((item) => (finalResult.push({id: item.id, name: item.name, ceo: item.ceo, score: item.score  })));
+  res.status(200).json(finalResult);
 }
 
+exports.updateDetails = async (req, res) => {
+  const ceo = req.body.ceo;
+  const id = req.body.id;
+  const result = await services.updateDetails(ceo, id);
+  res.status(200).json(result);
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,4 +3,5 @@ const detailsController = require('../controllers/detailsController');
 
 router.post('/api/save', detailsController.detailsController);
 router.get('/api/companies', detailsController.getCompanies);
+router.put('/api/update', detailsController.updateDetails);
 module.exports = router;
