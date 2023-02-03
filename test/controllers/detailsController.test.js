d9b801f2e49117d2bf7c0e597378489fd6831700
GauravPoosarla <poosarlagaurav@gmail.com>
2023-02-03 15:20:30 +0530

    feat: add tests for get by sector

--- test/controllers/detailsController.test.js
+++ test/controllers/detailsController.test.js
@@ -30,3 +30,65 @@ describe('post details', () => {
     }]);
   });
 });
+
+describe('post details', () => {
+  it('should return an error', async () => {
+    jest.spyOn(services, 'postDetails').mockRejectedValue('error');
+    const mockreq = {};
+    const mockres = {
+      json: jest.fn()
+    };
+    await controllers.detailsController(mockreq, mockres);
+    expect(mockres.json).toHaveBeenCalledWith('error');
+  });
+});
+
+describe('get companies', () => {
+  it('should return all companies in a sector', async () => {
+    jest.spyOn(services, 'getCompanies').mockResolvedValue([{
+      id: 1,
+      name: 'company1',
+      score: 0
+    },
+    {
+      id: 2,
+      name: 'company2',
+      score: 0
+    }]);
+    const mockreq = {
+      query: {
+        sector: 'sector1'
+      }
+    };
+    const mockres = {
+      json: jest.fn()
+    };
+    await controllers.getCompanies(mockreq, mockres);
+    expect(mockres.json).toHaveBeenCalledWith([{
+      id: 1,
+      name: 'company1',
+      score: 0
+    },
+    {
+      id: 2,
+      name: 'company2',
+      score: 0
+    }]);
+  });
+});
+
+describe('get companies', () => {
+  it('should return an error', async () => {
+    jest.spyOn(services, 'getCompanies').mockRejectedValue('error');
+    const mockreq = {
+      query: {
+        sector: 'sector1'
+      }
+    };
+    const mockres = {
+      json: jest.fn()
+    };
+    await controllers.getCompanies(mockreq, mockres);
+    expect(mockres.json).toHaveBeenCalledWith('error');
+  });
+});
